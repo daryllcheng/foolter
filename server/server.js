66595ddab44e74ec07ab225c9693749f577ae18f
Daryll <daryllcsh@gmail.com>
2017-12-21 22:14:47 -0800

    Prepare packagejson for heroku

--- server/server.js
+++ server/server.js
@@ -1,15 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import path from 'path';
 import data from './data/food_data.json';
 import filterFoodsByNutrients from './utilities/helpers';
 
 const app = express();
-const router = express.Router();
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.set('port', (process.env.PORT || 3001));
-app.use(router);
+
+const router = express.Router();
+
+const staticFiles = express.static(path.join(__dirname, '../../client/build'));
+app.use(staticFiles);
 
 router.get('/foods', (req, res) => {
   const foods = data.report.foods;
@@ -17,6 +19,11 @@ router.get('/foods', (req, res) => {
   res.json(filteredFoods);
 });
 
+app.use(router);
+
+app.use('/*', staticFiles);
+
+app.set('port', (process.env.PORT || 3001));
 app.listen(app.get('port'), () => {
   console.log(`Listening on ${ app.get('port') }`)
 })
